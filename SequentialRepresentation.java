0b6d76c74000f50d37e876bf11b784e2ddf4a618
Jack <jackryan1998@hotmail.com>
2019-08-15 13:48:09 +1000

    Update SequentialRepresentation.java

--- SequentialRepresentation.java
+++ SequentialRepresentation.java
@@ -13,18 +13,38 @@ public class SequentialRepresentation<T> implements BSPTree<T> {
      * Constructs empty graph.
      */
 
+    /*
+    parent	(i - 1) / 2
+    left	2 * i + 1
+    right	2 * i + 2
+    */
+
+   protected T[] binaryTreeArray;
+
     public SequentialRepresentation() {
-        T[] binaryTreeArray = (T[]) new Object[100];
+        binaryTreeArray = (T[]) new Object[100];
     } // end of SequentialRepresentation()
 
     @Override
     public void setRootNode(T nodeLabel) {
+        if (binaryTreeArray[0] != EMPTY_NODE){
+            binaryTreeArray [0] = nodeLabel;
+        } else {
+            System.out.println("Root already set");
+        }
 
     } // end of setRootNode()
 
     @Override
     public void splitNode(T srcLabel, T leftChild, T rightChild) {
         // Implement me!
+        //if node exists, then check if left and right children aren't already set
+        if(findNode(srcLabel)){
+        //    binaryTreeArray[]
+
+
+        }
+
     } // end of splitNode
 
     @Override
@@ -59,53 +79,6 @@ public class SequentialRepresentation<T> implements BSPTree<T> {
     public void printInPostorder(PrintWriter writer) {
         // Implement me!
     } // end of printInPostorder
-
-
-//    protected class Node{
-//        protected T vertLabel;
-//        protected Node rightChild, leftChild, parent;
-//
-//
-//        protected Node(T vertLabel) {
-//            this.vertLabel = vertLabel;
-//            this.leftChild = null;
-//            this.rightChild = null;
-//            this.parent = null;
-//        }
-//
-//        public T getVertLabel() {
-//            return vertLabel;
-//        }
-//
-//        public void setVertLabel(T vertLabel) {
-//            this.vertLabel = vertLabel;
-//        }
-//
-//        public Node getRightChild() {
-//            return rightChild;
-//        }
-//
-//        public void setRightChild(Node rightChild) {
-//            this.rightChild = rightChild;
-//        }
-//
-//        public Node getLeftChild() {
-//            return leftChild;
-//        }
-//
-//        public void setLeftChild(Node leftChild) {
-//            this.leftChild = leftChild;
-//        }
-//
-//        public Node getParent() {
-//            return parent;
-//        }
-//
-//        public void setParent(Node parent) {
-//            this.parent = parent;
-//        }
-
-    }
 
 } // end of class SequentialRepresentation
 
